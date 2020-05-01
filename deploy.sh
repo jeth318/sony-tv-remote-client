9a0b9eec1f61d49b49a99ae98b90353b3124676d
jeth318 <jesper.thornberg@me.com>
2020-05-01 18:05:28 +0200

    Add deploy-script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+cd /home/pi/Apps/sony-tv-remote
+echo "Stashing lock files"
+git stash
+
+echo "Dropping stash"
+git stash drop
+
+echo "Pulling from Master"
+git pull origin master
+echo "Pulled successfully from master"
+
+echo "Installing dependencies..."
+rm -rf node_modules
+npm install
+echo "Dependencies were installed"
+
+echo "Rebuilding application"
+npm run build
+
+echo "Rebuild OK"
+echo "Deployment complete, and it was a success!"
+exit 0
