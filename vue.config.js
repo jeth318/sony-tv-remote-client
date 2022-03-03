9350bfa3f228539d10b951367461416a29c508ab
jeth318 <jesper.thornberg@me.com>
2022-03-03 22:11:25 +0100

    Fix puclic path

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,12 @@
 // vue.config.js
 const path = require('path')
-console.log(process.env.NODE_ENV)
 const isLocalDev = () => process.env.VUE_APP_LOCALDEV
 const isProductionBuild = () => process.env.NODE_ENV === 'production'
 const isDevelopmentBuild = () => process.env.NODE_ENV === 'production'
 
 const getPublicPath = () => {
-    if (isProductionBuild() || isDevelopmentBuild()) {
-        if (isLocalDev()) {
-            return path.resolve(__dirname, 'dist')
-        }
-        return '/sony-tv-remote'
+    if (isLocalDev() && (isProductionBuild() || isDevelopmentBuild())) {
+        return path.resolve(__dirname, 'dist')
     }
     return '/'
 }
